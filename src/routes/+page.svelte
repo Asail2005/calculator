159434577b76f64c096d77fcaf1f2cb0d6372404
Asail <asail2005aa.com>
2024-06-22 00:18:10 +0300

    shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,27 +4,31 @@
     </title>
 </svelte:head>
 
-<div class="bg-white h-[30rem] w-[20rem] rounded-3xl 
- grid grid-cols-4 gap-4" >
-  <button >%</button>
-  <button>√</button>
-  <button>CE</button>
-  <button >C</button>
-  <button>7</button>
-  <button>8</button>
-  <button>9</button>
-  <button>-</button>
-  <button>4</button>
-  <button>5</button>
-  <button>6</button>
-  <button>÷</button>
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button>×</button>
-  <button>.</button>
-  <button>0</button>
-  <button>=</button>
-  <button>+</button>
+<div class="bg-white rounded-3xl font-semibold shadow-2xl
+ grid grid-cols-4 gap-4 p-7" >
+ <div class="bg-blue-500 rounded-full col-span-4 
+ h-14 flex items-cente p-3 mb-1  text-2xl text-white font-semibold">
+58676
+</div>
+  <button class=" text-slate-500 bg-[#f3f6fc]">%</button>
+  <button class="  text-slate-500 bg-[#f3f6fc]">√</button>
+  <button class="  text-slate-500 bg-[#f3f6fc]">CE</button>
+  <button class="  text-white bg-[#1f2d54]">C</button>
+  <button class="  text-slate-600">7</button>
+  <button class="  text-slate-600">8</button>
+  <button class="  text-slate-600">9</button>
+  <button class="  text-white bg-[#fd3f59]">-</button>
+  <button class="  text-slate-600">4</button>
+  <button class="  text-slate-600">5</button>
+  <button class="  text-slate-600">6</button>
+  <button class="  text-white bg-[#2285fd]">÷</button>
+  <button class="  text-slate-600">1</button>
+  <button class="  text-slate-600">2</button>
+  <button class="  text-slate-600">3</button>
+  <button class="  text-white bg-[#f9c80e]">×</button>
+  <button class="  text-slate-600">.</button>
+  <button class="  text-slate-600">0</button>
+  <button class="  text-slate-500 bg-[#f3f6fc]">=</button>
+  <button class="  text-white bg-[#63dc74]">+</button>
 
 </div>
